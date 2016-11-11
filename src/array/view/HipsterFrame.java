ad8485ff09d4707ce9bb1c906e579bc61057ec8d
PayAsian <ptre8426@csddocs.org>
2016-11-11 11:10:38 -0700

    yeah

--- src/array/view/HipsterFrame.java
+++ src/array/view/HipsterFrame.java
@@ -3,25 +3,26 @@ package array.view;
 import javax.swing.JFrame;
 import array.controller.ArrayController;
 import java.awt.Dimension;
+import array.view.HipsterPanel;
 
 public class HipsterFrame extends JFrame
 {
 
 	private ArrayController baseController;
-	private HipsterPanel basePanel;
+	private HipsterPanel BasePanel;
 	
 	public HipsterFrame(ArrayController baseController)
 	{
 		super();
 		this.baseController = baseController;
-		basePanel = new HipsterPanel(baseController);
+		BasePanel = new HipsterPanel(baseController);
 		
 		setupFrame();
 	}
 	
 	private void setupFrame()
 	{
-		this.setContentPane(basePanel);
+		this.setContentPane(BasePanel);
 		this.setTitle("Hipsters 2016");
 		this.setSize(new Dimension(600,400));
 		this.setResizable(false);
--- src/array/view/HipsterPanel.java
+++ src/array/view/HipsterPanel.java
@@ -1,6 +1,51 @@
 package array.view;
 
+import java.awt.color.*;
+import javax.swing.*;
+import array.controller.ArrayController;
+
 public class HipsterPanel
 {
+	private ArrayController baseController;
+	private JButton myButton;
+	private JComboBox dropDown;
+	private JLabel wordsLabel;
+	private SpringLayout baseLayout;
+	
+	public HipsterPanel(ArrayController baseController)
+	{
+		this.baseController = baseController;
+		myButton = new JButton("Click here!!");
+		wordsLabel = new JLabel("such words!!");
+		baseLayout = new SpringLayout();
+		dropDown = new JComboBox(baseController.getWords());
+		
+		setupPanel();
+		setupLayout();
+		setupListeners();
+	}
 
+	private void setupPanel()
+	{
+		this.setLayout(baseLayout);
+		this.setBackground(Color.ORANGE);
+		this.add(myButton);
+		this.add(wordsLabel);
+		this.add(dropDown);
+	}
+	private void setupLayout()
+	{
+		
+	}
+	private void setupListeners()
+	{
+		dropDown.addActionListener(new ActionListener);
+		{
+			public void actionPerform
+		
+		{
+			
+		});
+	}
+	}	
 }
